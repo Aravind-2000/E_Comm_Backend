cf5fc4235af7506d8107786b2d96bcb6c3c3c80d
Aravind-2000 <varavind2000@gmail.com>
2022-11-26 09:36:46 +0530

    added controllers for product,user,address

--- src/main/java/com/example/ecomm/controller/ProductController.java
+++ src/main/java/com/example/ecomm/controller/ProductController.java
@@ -3,9 +3,13 @@ package com.example.ecomm.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +19,7 @@ import com.example.ecomm.service.ProductService;
 
 @RestController
 @RequestMapping("/product")
-@CrossOrigin
+@CrossOrigin(origins ="null", allowedHeaders = "*")
 public class ProductController {
 
     @Autowired
@@ -29,8 +33,18 @@ public class ProductController {
 
 
     @PostMapping("/add")
-    public String add(@RequestBody Product product){
+    public String add(@RequestBody Product product) {
         return productService.saveProduct(product);
+    }
+    
+    @PutMapping("/addtocart/{productid}/{cartid}")
+    public ResponseEntity<?> addToCart(@PathVariable int productid, @PathVariable int cartid) {
+        return ResponseEntity.ok(productService.addProductToCart(productid, cartid));
+    }
+    
+    @DeleteMapping("/removefromcart/{productid}/{cartid}")
+    public ResponseEntity<?> removeFromCart(@PathVariable int productid, @PathVariable int cartid) {
+        return ResponseEntity.ok(productService.removeProductFromCart(productid, cartid));
     }
     
 }
