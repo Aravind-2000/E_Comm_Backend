0a48942bf19b766331910b983d67206ded89f8bb
Aravind-2000 <varavind2000@gmail.com>
2022-11-27 20:00:00 +0530

    added adding&removing products from cart services

--- src/main/java/com/example/ecomm/service/ProductService.java
+++ src/main/java/com/example/ecomm/service/ProductService.java
@@ -10,44 +10,41 @@ import com.example.ecomm.entity.Product;
 import com.example.ecomm.repository.CartRepository;
 import com.example.ecomm.repository.ProductRepository;
 
-
 @Service
 public class ProductService {
-	
+
 	@Autowired
 	private ProductRepository productRepository;
 
 	@Autowired
 	private CartRepository cartRepository;
-	
-	
-	public List<Product> getAllProducts(){
+
+	public List<Product> getAllProducts() {
 		return productRepository.findAll();
 	}
-	
+
 	public Product getProduct(int pId) {
 		return productRepository.getReferenceById(pId);
 	}
-	
+
 	public String saveProduct(Product product) {
 		productRepository.save(product);
 		return "Product Added successfully";
 	}
-	
+
 	public String deleteProduct(int pId) {
 		productRepository.deleteById(pId);
 		return "Product deleted successfully";
 	}
 
-
 	public String addProductToCart(int productId, int cartId) {
-		if (productRepository.findById(productId).isPresent()) {
-			if (cartRepository.findById(cartId).isPresent()) {
+		if (cartRepository.findById(cartId).isPresent()) {
+			if (productRepository.findById(productId).isPresent()) {
 				Product product = productRepository.getReferenceById(productId);
 				Cart cart = cartRepository.getReferenceById(cartId);
-				if (!cart.getProductsInCart().contains(product)) {
-					cart.addProduct(product);
-					cartRepository.save(cart);
+				if (!product.getAddedCarts().contains(cart)) {
+					product.addProduct(cart);
+					productRepository.save(product);
 					return "Product Added in cart Successfully";
 				} else {
 					return "Product Already in cart";
@@ -60,11 +57,9 @@ public class ProductService {
 	public String removeProductFromCart(int productId, int cartId) {
 		Product product = productRepository.getReferenceById(productId);
 		Cart cart = cartRepository.getReferenceById(cartId);
-		cart.removeProduct(product);
-		cartRepository.save(cart);
+		product.removeProduct(cart);
+		productRepository.save(product);
 		return "Product Removed from cart successfully";
 	}
-	
-	
 
 }
