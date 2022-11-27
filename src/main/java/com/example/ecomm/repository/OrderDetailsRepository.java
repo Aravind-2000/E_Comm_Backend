b86041fbc9ca8bc4b207857a02c633c1ca7cbf17
Aravind-2000 <varavind2000@gmail.com>
2022-11-27 20:01:04 +0530

    Changed return type of  getByUserId method

--- src/main/java/com/example/ecomm/repository/OrderDetailsRepository.java
+++ src/main/java/com/example/ecomm/repository/OrderDetailsRepository.java
@@ -1,11 +1,13 @@
 package com.example.ecomm.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.ecomm.entity.OrderDetails;
 
 public interface OrderDetailsRepository extends JpaRepository<OrderDetails, Integer> {
 
-    public OrderDetails getByUserId(int userId);
+    public List<OrderDetails> getByUserId(int userId);
 
 }
