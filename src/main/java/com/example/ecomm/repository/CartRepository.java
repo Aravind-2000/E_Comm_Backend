73051ea424f8e93a3138a30c38d3cfddd04959d7
Aravind-2000 <varavind2000@gmail.com>
2022-11-27 20:00:33 +0530

    added getByUserId & getByCartId methods

--- src/main/java/com/example/ecomm/repository/CartRepository.java
+++ src/main/java/com/example/ecomm/repository/CartRepository.java
@@ -1,9 +1,17 @@
 package com.example.ecomm.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.ecomm.entity.Cart;
 
 public interface CartRepository extends JpaRepository<Cart, Integer> {
+
+    @Query(value = "select * from cart where user_id = :userId", nativeQuery = true)
+    public Cart getByUserId(@Param("userId") int userId);
+
+    @Query(value = "select id from cart where user_id = :userId", nativeQuery = true)
+    public int getCartId(@Param("userId") int userId);
 
 }
