c6670b6d8ee4fd700e8b6cffecc4138dbd92407d
Aravind-2000 <varavind2000@gmail.com>
2022-12-04 18:32:04 +0530

    added delete by id method

--- src/main/java/com/example/ecomm/controller/ProductCategoryController.java
+++ src/main/java/com/example/ecomm/controller/ProductCategoryController.java
@@ -3,12 +3,7 @@ package com.example.ecomm.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.ecomm.entity.ProductCategory;
 import com.example.ecomm.service.ProductCategoryService;
@@ -29,6 +24,11 @@ public class ProductCategoryController {
     @PostMapping("/add")
     public String add(@RequestBody ProductCategory category) {
         return productCategoryService.addCategory(category);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String delete(@PathVariable int id){
+        return productCategoryService.deleteCategory(id);
     }
 
 }
