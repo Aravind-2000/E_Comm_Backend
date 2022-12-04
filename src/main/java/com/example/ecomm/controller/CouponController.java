85c0c6064dd629356cce54ca23ab4e86384da7b8
Aravind-2000 <varavind2000@gmail.com>
2022-12-04 19:02:02 +0530

    added method getDiscountByCouponCode in coupon controller and repository

--- src/main/java/com/example/ecomm/controller/CouponController.java
+++ src/main/java/com/example/ecomm/controller/CouponController.java
@@ -2,12 +2,7 @@ package com.example.ecomm.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.ecomm.entity.Coupon;
 import com.example.ecomm.entity.Event;
@@ -38,6 +33,11 @@ public class CouponController {
         }
         couponRepository.save(coupon);
         return ResponseEntity.ok("Coupon Added");
+    }
+
+    @GetMapping("/getcoupondiscount/{couponCode}")
+    public int getDiscount(@PathVariable String couponCode){
+        return couponRepository.getCouponDiscount(couponCode);
     }
 
 }
--- src/main/java/com/example/ecomm/repository/CouponRepository.java
+++ src/main/java/com/example/ecomm/repository/CouponRepository.java
@@ -3,7 +3,12 @@ package com.example.ecomm.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.ecomm.entity.Coupon;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CouponRepository extends JpaRepository<Coupon, Integer> {
+
+    @Query(value = "select coupon_discount from coupon where coupon_code = :couponCode ", nativeQuery = true)
+    int getCouponDiscount(@Param("couponCode") String couponCode);
 
 }
