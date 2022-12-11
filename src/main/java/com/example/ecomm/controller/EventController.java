8afaa47d5d395b9568af773aa2a8014b0909ed8d
Aravind-2000 <varavind2000@gmail.com>
2022-12-11 13:17:05 +0530

    Modified the del method based upon the coup atchd

--- src/main/java/com/example/ecomm/controller/EventController.java
+++ src/main/java/com/example/ecomm/controller/EventController.java
@@ -1,10 +1,14 @@
 package com.example.ecomm.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.example.ecomm.entity.Coupon;
 import com.example.ecomm.entity.Event;
+import com.example.ecomm.repository.CouponRepository;
 import com.example.ecomm.repository.EventRepository;
 
 @RestController
@@ -14,6 +18,9 @@ public class EventController {
 
     @Autowired
     private EventRepository eventRepository;
+
+    @Autowired
+    private CouponRepository couponRepository;
 
     @GetMapping("/getall")
     public ResponseEntity<?> getAllEvents() {
@@ -26,20 +33,32 @@ public class EventController {
         return ResponseEntity.ok("Event Added");
     }
 
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteEvent(@PathVariable int id) {
+        List<Coupon> coupons = couponRepository.findAll();
+        for (Coupon coupon : coupons) {
+            if (coupon.getEventId() == id) {
+                return ResponseEntity.badRequest().body("You cannot delete this event, it is associated with a coupon");
+            }
+        }
+        eventRepository.deleteById(id);
+        return ResponseEntity.ok("Event deleted");
+    }
+
     @PatchMapping("/edit/{id}")
-    public ResponseEntity<?> editEvent(@PathVariable int id, @RequestBody Event newEvent){
+    public ResponseEntity<?> editEvent(@PathVariable int id, @RequestBody Event newEvent) {
 
         Event oldEvent = eventRepository.getReferenceById(id);
 
-        if(newEvent.getEventName() != null){
+        if (newEvent.getEventName() != null) {
             oldEvent.setEventName(newEvent.getEventName());
         }
 
-        if(newEvent.getEventStartDate() != null){
+        if (newEvent.getEventStartDate() != null) {
             oldEvent.setEventStartDate(newEvent.getEventStartDate());
         }
 
-        if(newEvent.getEventEndDate() != null){
+        if (newEvent.getEventEndDate() != null) {
             oldEvent.setEventEndDate(newEvent.getEventEndDate());
         }
 
