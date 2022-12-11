60d8a142fb8288b9d53112d8fe946263d02f85b0
Aravind-2000 <varavind2000@gmail.com>
2022-12-11 13:18:34 +0530

    added method for getting avail prods

--- src/main/java/com/example/ecomm/repository/ProductRepository.java
+++ src/main/java/com/example/ecomm/repository/ProductRepository.java
@@ -1,5 +1,7 @@
 package com.example.ecomm.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,4 +13,6 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
     @Query(value = "select * from product where product_id = :productId", nativeQuery = true)
     public Product geProduct(@Param("productId") int productId);
 
+    @Query(value = "select * from product where is_available = 1", nativeQuery = true)
+    public List<Product> availableProducts();
 }
